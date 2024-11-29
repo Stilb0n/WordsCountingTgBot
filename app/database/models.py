f3762c6215a3749d59fcccb2bb75325eb4e2aac2
Stilb0n <stilbon01@gmail.com>
2024-11-29 15:11:08 +0700

    models.py updated
    при запуске программы происходит  подключение и создание  базы данных (создание происходит если бд отсутствует)

--- app/database/models.py
+++ app/database/models.py
@@ -1,15 +1,14 @@
 from sqlalchemy import BigInteger, String, ForeignKey
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
 from sqlalchemy.ext.asyncio import AsyncAttrs, async_sessionmaker, create_async_engine
+import asyncio
 
-engine = create_async_engine(url='sqlite+aiosqlite:///db.sqlite3')
+engine = create_async_engine(url='postgresql+asyncpg://postgres:12345@localhost/WordCountingTgBot_db')
 
 async_session = async_sessionmaker(engine)
 
-
 class Base(AsyncAttrs, DeclarativeBase):
     pass
-
 
 class User(Base):
     __tablename__ = 'users'
@@ -18,23 +17,13 @@ class User(Base):
     tg_id = mapped_column(BigInteger)
 
 
-class Category(Base):
-    __tablename__ = 'categories'
-    
-    id: Mapped[int] = mapped_column(primary_key=True)
-    name: Mapped[str] = mapped_column(String(25))
-
-
-class Item(Base):
-    __tablename__ = 'items'
-    
-    id: Mapped[int] = mapped_column(primary_key=True)
-    name: Mapped[str] = mapped_column(String(25))
-    description: Mapped[str] = mapped_column(String(120))
-    price: Mapped[int] = mapped_column()
-    category: Mapped[int] = mapped_column(ForeignKey('categories.id'))
-
 
 async def async_main():
-    async with engine.begin() as conn:
-        await conn.run_sync(Base.metadata.create_all)
+    try:
+        async with engine.begin() as conn:
+            await conn.run_sync(Base.metadata.create_all)
+    except Exception as e:
+        print(f'An error occurred: {e}')
+
+if __name__ == "main":
+    asyncio.run(async_main())
